88661baaa17dc8ca7f394529ab7313dcebc414e9
Kuwar20 <kuwarx1@gmail.com>
2023-12-14 17:48:46 +0530

    create user model

---
+++ server/models/userSchema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+//create user schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// create model
+const User = mongoose.model("User", userSchema);
+
+export default User;
