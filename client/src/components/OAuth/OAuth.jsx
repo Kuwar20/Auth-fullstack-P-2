9cf25bc6484078b4a2015d4d1388718e15cce43a
Kuwar20 <kuwarx1@gmail.com>
2023-12-18 20:05:47 +0530

    complete update user functionality

--- client/src/components/OAuth/OAuth.jsx
+++ client/src/components/OAuth/OAuth.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
-import { app } from '../../firebase';
-import { useDispatch } from 'react-redux';
-import { signInSuccess } from '../../redux/user/userSlice';
+import React from "react";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "../../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleGoogleClick = async () => {
         try {
@@ -13,27 +15,34 @@ const OAuth = () => {
             const auth = getAuth(app);
             const result = await signInWithPopup(auth, provider);
             // console.log(result);
-            const res = await fetch('api/auth/google', {
-                method: 'POST',
+            const res = await fetch("api/auth/google", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
                     name: result.user.displayName,
                     email: result.user.email,
                     photo: result.user.photoURL,
                 }),
-            })
+            });
             const data = await res.json();
             dispatch(signInSuccess(data));
+            navigate("/");
         } catch (error) {
             console.log("could not login with google", error);
         }
-    }
+    };
 
     return (
-        <button type='button' onClick={handleGoogleClick} className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'>Continue with google</button>
-    )
-}
+        <button
+            type="button"
+            onClick={handleGoogleClick}
+            className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
+        >
+            Continue with google
+        </button>
+    );
+};
 
-export default OAuth
+export default OAuth;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,16 +7,25 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
+  const dispatch = useDispatch();
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
   const [imagePercent, setImagePercent] = useState(0);
   //console.log(imagePercent);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
-  console.log(formData);
-  const { currentUser } = useSelector((state) => state.user);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  //console.log(formData);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
 
   useEffect(() => {
     if (image) {
@@ -45,10 +54,38 @@ const Profile = () => {
       }
     );
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (err) {
+      dispatch(updateUserFailure(err));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
           ref={fileRef}
@@ -65,7 +102,9 @@ const Profile = () => {
         />
         <p className="text-sm self-center">
           {imageError ? (
-            <span className="text-red-700">Image upload failed ( keep file size under 2 MB )</span>
+            <span className="text-red-700">
+              Image upload failed ( keep file size under 2 MB )
+            </span>
           ) : imagePercent > 0 && imagePercent < 100 ? (
             <span className="text-slate-700">{`Uploading:  ${imagePercent} %`}</span>
           ) : imagePercent === 100 ? (
@@ -80,6 +119,7 @@ const Profile = () => {
           id="username"
           placeholder="Username"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           defaultValue={currentUser.email}
@@ -87,21 +127,25 @@ const Profile = () => {
           id="email"
           placeholder="Email"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           type="password"
           id="password"
           placeholder="Password"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
+      <p className="text-green-700 mt-5">{updateSuccess && 'User is updated successfully!'}</p>
     </div>
   );
 };
