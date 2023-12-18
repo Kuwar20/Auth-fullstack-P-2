df392ef502ed317282bbe1ec5bbdc5a3972827a7
Kuwar20 <kuwarx1@gmail.com>
2023-12-19 00:02:11 +0530

    complete about and home pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    
+    <div className="min-h-screen bg-gray-100">
+    <div className="bg-slate-500 p-4 text-white text-center">
+      <h1 className="text-4xl font-bold">Mern Auth</h1>
+      <p className="text-lg">Cutting-edge technology for seamless experiences</p>
+    </div>
+    
+    <div className="container mx-auto mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">Our Features</h2>
+          <ul className="list-disc list-inside">
+            <li>Secure Firebase Authentication</li>
+            <li>Convenient Google Sign-In with OAuth</li>
+            <li>Reliable Firebase File Storage</li>
+            <li>Efficient Redux Toolkit and Redux Persist</li>
+          </ul>
+        </div>
 
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">Our Vision</h2>
+          <p className="text-lg">
+            At Mern Auth, we envision a future where technology seamlessly integrates with your daily life. Our commitment to excellence drives us to provide a platform that not only meets but exceeds your expectations.
+          </p>
+        </div>
+      </div>
+    </div>
+    
+    <div className="bg-slate-500 p-4 text-white text-center mt-8">
+      <p className="text-lg">Join me on this exciting journey where innovation and user satisfaction converge. Discover the difference today.</p>
+    </div>
+    </div>
+    
+      )
+    }
 export default About
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to my  Auth App</h1>
+      <p className='mb-4 text-slate-700'>This is a full-stack web application built with the MERN
+        (MongoDB, Express, React , Node.js) stack. It
+        includes authentication and log out, and provides access to
+        protected routes for authenticated users.
+      </p>
+      <p>
+      Our platform leverages Firebase Authentication for robust user identity management, offering a secure and hassle-free sign-up and login experience. Harness the power of OAuth for Google Sign-In to enable users to access your application effortlessly with their Google credentials.
+      </p>
+      <br />
+      <p>
+      With Firebase File Storage, our web app ensures reliable and scalable storage solutions for your user-generated content. Effortlessly upload, download, and manage files while ensuring data integrity and security. Firebase's powerful infrastructure guarantees high availability and low-latency access to your stored data.
+      </p>
+      <br />
+      <p>
+      Embrace efficient state management with Redux Toolkit and Redux Persist. Redux Toolkit simplifies the process of managing the application state, ensuring a predictable and maintainable codebase. Redux Persist adds a layer of persistence, allowing users to seamlessly continue their experience across sessions.
+      </p>
+    </div>
   )
 }
 
